b570d27419e235e29dee82bd0872a804fa984591
Hardik <hardikchhabra3007@gmail.com>
2023-09-24 16:48:26 +0530

    attempt to integrate js and flask

---
+++ webrtc-firebase-demo-main/main.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, redirect, request, session
+from sqlalchemy import create_engine, text
+import os
+
+engine = create_engine("mysql+pymysql://SPARTAN_suddenlyas:8eeacd533d88e5312f8cf57eec79f173bddf8e01@j5l.h.filess.io:3307/SPARTAN_suddenlyas?charset=utf8mb4")
+
+app = Flask(__name__)
+app.secret_key = "best_team"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+def load_users_from_db():
+      with engine.connect() as conn:
+        result = conn.execute(text('select * from users'))
+        USERS = []
+        for row in result.all():
+          USERS.append(row._mapping)
+        return USERS
+
+
+def User(username, password):
+    users = load_users_from_db()
+    for i in users:
+        if i['name'] == username and i['password'] == password:
+            return True      
+    return False
+
+
+@app.route('/dashboard')
+def dashboard(username = "Developer"):
+    if 'user_id' in session:
+        return render_template('dashboard.html', name = username)
+    return redirect('/')
+        
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect('/')
+
+
+@app.route('/createroom')
+def createroom():
+    if 'user_id' in session:
+        os.system("npm run dev")
+        return redirect('http://localhost:3000/')
+    return redirect('/')
+
+
+@app.route('/joinroom')
+def joinroom():
+    if 'user_id' in session:
+        return render_template('joinRoom.html')
+    return redirect('/')
+
+
+@app.route('/forgot_pwd')
+def forgot_pwd():
+    return render_template('forgot_pwd.html')
+
+
+@app.route('/signIn', methods=['POST'])
+def signin():
+    if request.method == 'POST':
+        username = request.form.get('user_id')
+        password = request.form.get('password')
+        user = User(username, password)
+        if user:
+            session['user_id'] = 0
+            return dashboard(username)
+        else:
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+
+@app.route('/joinroomwithid', methods=['POST'])
+def joinroomwithid():
+    if 'user_id' in session:
+        if request.method == 'POST':
+            room_id = request.form.get('room_id')
+            return redirect(f'/createroom?roomID={room_id}')
+
+
+if __name__ == '__main__':
+    print("I am inside if")
+    app.run()
---
+++ webrtc-firebase-demo-main/static/js/login.js
@@ -0,0 +1,124 @@
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+
+
+signInBtn.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+
+
+
+
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "star",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
+        polygon: {
+          nb_sides: 5
+        }
+      },
+      opacity: {
+        value: 0.4,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: false,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "grab"
+        },
+        onclick: {
+          enable: false,
+          mode: "push"
+        },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 150,
+          line_linked: {
+            opacity: 0.2
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 200
+        },
+        push: {
+          particles_nb: 4
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true
+  });
